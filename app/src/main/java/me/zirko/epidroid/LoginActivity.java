136bd9de3553a8a1227b0b0f1ee1a1c7f458d0af
rémi <remirobert33530@gmail.com>
2015-01-12 22:46:41 +0100

    Add fragment to the LoginActivity. Add connection to the API for the user connection with Volley.

--- app/src/main/java/me/zirko/epidroid/LoginActivity.java
+++ app/src/main/java/me/zirko/epidroid/LoginActivity.java
@@ -4,12 +4,25 @@ import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import me.zirko.epidroid.network.VolleySingleton;
 
 
 public class LoginActivity extends ActionBarActivity {
@@ -23,8 +36,8 @@ public class LoginActivity extends ActionBarActivity {
                     .add(R.id.container, new PlaceholderFragment())
                     .commit();
         }
-    }
 
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -56,10 +69,57 @@ public class LoginActivity extends ActionBarActivity {
         public PlaceholderFragment() {
         }
 
+        public void makeRequest() {
+            StringRequest postRequest = new StringRequest(Request.Method.POST, "https://epitech-api.herokuapp.com/login",
+                    new Response.Listener<String>()
+                    {
+                        @Override
+                        public void onResponse(String response) {
+                            // response
+                            Log.d("Response", response);
+                        }
+                    },
+                    new Response.ErrorListener()
+                    {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            // error
+                            Log.d("Error.Response", error.toString());
+                        }
+                    }
+            ) {
+                @Override
+                protected Map<String, String> getParams()
+                {
+                    Map<String, String>  params = new HashMap<String, String>();
+                    params.put("login", "robert_r");
+                    params.put("password", "fl5>[dWn");
+                    return params;
+                }
+            };
+
+            VolleySingleton.getInstance(getActivity()).addToRequestQueue(postRequest);
+        }
+
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_login, container, false);
+
+            View rootView =  inflater.inflate(R.layout.fragment_login, container, false);
+
+            Button buttonConnection = (Button)rootView.findViewById(R.id.buttonConnection);
+
+            buttonConnection.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+
+
+                    makeRequest();
+                }
+            });
+
+
             return rootView;
         }
     }
