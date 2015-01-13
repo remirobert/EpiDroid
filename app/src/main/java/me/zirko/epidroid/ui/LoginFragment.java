3f0aba1d17b0b315c8814ddeb59e4430d6307e83
Guillaume Fillon <guillaume@posteo.de>
2015-01-13 16:24:09 +0100

    Add auto-connection
    Store the token on connection and re-use it if available

--- app/src/main/java/me/zirko/epidroid/ui/LoginFragment.java
+++ app/src/main/java/me/zirko/epidroid/ui/LoginFragment.java
@@ -1,6 +1,8 @@
 package me.zirko.epidroid.ui;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
@@ -23,14 +25,38 @@ import me.zirko.epidroid.model.Account;
 import me.zirko.epidroid.network.GsonRequest;
 import me.zirko.epidroid.network.VolleySingleton;
 
-public class LoginFragment extends Fragment implements Response.Listener<Account>, Response.ErrorListener{
+public class LoginFragment extends Fragment
+        implements Response.Listener<Account>, Response.ErrorListener {
+
+    private static final String TAG = LoginFragment.class.getSimpleName();
+    private static final String PREFERENCE_CREDENTIALS = "preference:credentials";
+    private static final String PREFERENCE_CREDENTIALS_TOKEN = "preference:credentials";
 
     public LoginFragment() {
     }
 
     @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        SharedPreferences sharedPref = getActivity().getSharedPreferences(
+                PREFERENCE_CREDENTIALS, Context.MODE_PRIVATE);
+        String token = sharedPref.getString(PREFERENCE_CREDENTIALS_TOKEN, null);
+
+        if (token != null) {
+            Intent intent = new Intent(getActivity(), HomeActivity.class);
+            intent.putExtra("token", token);
+            startActivity(intent);
+            getActivity().finish();
+        }
+    }
+
+    @Override
     public void onResponse(Account user) {
-        Log.i("USER DEBUG", "token : " + user.getToken());
+        SharedPreferences sharedPref = getActivity().getSharedPreferences(
+                PREFERENCE_CREDENTIALS, Context.MODE_PRIVATE);
+        sharedPref.edit().putString(PREFERENCE_CREDENTIALS_TOKEN, user.getToken()).commit();
+
         Intent intent = new Intent(getActivity(), HomeActivity.class);
         intent.putExtra("token", user.getToken());
         startActivity(intent);
@@ -40,40 +66,41 @@ public class LoginFragment extends Fragment implements Response.Listener<Account
     @Override
     public void onErrorResponse(VolleyError volleyError) {
         Toast.makeText(getActivity(), "Error connection", Toast.LENGTH_SHORT).show();
-        Log.e("ERROR REQUEST", "error : "+  volleyError.toString());
-    }
-
-    private void makeRequest() {
-        Map<String, String>  params = new HashMap<String, String>();
-        TextView loginTextView = (TextView)getView().findViewById(R.id.login_username);
-        TextView passwordTextView = (TextView)getView().findViewById(R.id.login_password);
-
-        params.put("login", loginTextView.getText().toString());
-        params.put("password", passwordTextView.getText().toString());
-
-        VolleySingleton.getInstance(getActivity()).addToRequestQueue(new GsonRequest<>(
-                Request.Method.POST,"https://epitech-api.herokuapp.com/login", Account.class, this, this, params));
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        View rootView =  inflater.inflate(R.layout.fragment_login, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_login, container, false);
 
-        final ActionProcessButton processButton = (ActionProcessButton)rootView.findViewById(R.id.login_connect);
+        final ActionProcessButton processButton = (ActionProcessButton) rootView.findViewById(R.id
+                .login_connect);
 
         //Button buttonConnection = (Button)rootView.findViewById(R.id.buttonConnection);
 
         processButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-               makeRequest();
+                makeRequest();
                 processButton.setProgress(30);
                 //processButton.setMode(ActionProcessButton.Mode.PROGRESS);
             }
         });
 
         return rootView;
+    }
+
+    private void makeRequest() {
+        Map<String, String> params = new HashMap<String, String>();
+        TextView loginTextView = (TextView) getView().findViewById(R.id.login_username);
+        TextView passwordTextView = (TextView) getView().findViewById(R.id.login_password);
+
+        params.put("login", loginTextView.getText().toString());
+        params.put("password", passwordTextView.getText().toString());
+
+        VolleySingleton.getInstance(getActivity()).addToRequestQueue(new GsonRequest<>(
+                Request.Method.POST, "https://epitech-api.herokuapp.com/login", Account.class,
+                this, this, params));
     }
 }
