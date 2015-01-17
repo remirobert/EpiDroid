e0010801a26e03d4e4dcc338aaf0bf4f8bc49253
Guillaume Fillon <guillaume@posteo.de>
2015-01-17 17:33:32 +0100

    Update login view

--- app/src/main/java/me/zirko/epidroid/ui/LoginActivity.java
+++ app/src/main/java/me/zirko/epidroid/ui/LoginActivity.java
@@ -1,7 +1,10 @@
 package me.zirko.epidroid.ui;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.Window;
+import android.view.WindowManager;
 
 import me.zirko.epidroid.R;
 
@@ -11,6 +14,14 @@ public class LoginActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.setStatusBarColor(getResources().getColor(R.color.theme_primary_dark));
+        }
+
         setContentView(R.layout.activity_login);
 
         if (savedInstanceState == null) {
