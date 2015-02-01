0c3c9cd993c0cdd0e3aebe252118ad454a4e4fe0
rémi <remirobert33530@gmail.com>
2015-02-01 11:15:37 +0100

    add mark list hashmap and listView display

--- app/src/main/java/me/zirko/epidroid/ui/MarkFragment.java
+++ app/src/main/java/me/zirko/epidroid/ui/MarkFragment.java
@@ -7,23 +7,28 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.HashMap;
+
 import me.zirko.epidroid.R;
-import me.zirko.epidroid.model.ListModuleItem;
-import me.zirko.epidroid.model.Marks;
-import me.zirko.epidroid.model.ModuleList;
+import me.zirko.epidroid.model.Mark.Note;
+import me.zirko.epidroid.model.Mark.Notes;
 import me.zirko.epidroid.network.GsonRequest;
 import me.zirko.epidroid.network.VolleySingleton;
 
-public class MarkFragment extends Fragment implements Response.Listener<Marks>, Response.ErrorListener{
+public class MarkFragment extends Fragment implements Response.Listener<Notes>, Response.ErrorListener{
 
     private String mToken;
     private static final String TAG = ModuleFragment.class.getSimpleName();
     private ArrayAdapter mAdapter;
     private static String API_ROUTE = "/marks";
+    private HashMap<String, ArrayList> moduleMap;
 
     public MarkFragment() {}
 
@@ -34,7 +39,7 @@ public class MarkFragment extends Fragment implements Response.Listener<Marks>, 
         if (getArguments() != null) {
             mToken = getArguments().getString("token");
         }
-        Log.i(TAG, "create fragments");
+        Log.i(TAG, "create fragments ok");
         fetchMarks();
     }
 
@@ -42,7 +47,7 @@ public class MarkFragment extends Fragment implements Response.Listener<Marks>, 
         String url = API_ROUTE + "?token=" + mToken;
 
         VolleySingleton.getInstance(getActivity()).addToRequestQueue(new GsonRequest<>(
-                url, Marks.class, this, this));
+                url, Notes.class, this, this));
     }
 
     @Override
@@ -57,7 +62,40 @@ public class MarkFragment extends Fragment implements Response.Listener<Marks>, 
     }
 
     @Override
-    public void onResponse(final Marks marks) {
-        Log.i(TAG, "debug + " + marks);
+    public void onResponse(final Notes marks) {
+        Log.i(TAG, "get notes okay");
+
+        moduleMap = new HashMap<String, ArrayList>();
+        for (Note currentNote: marks.getNotes()) {
+            if (!moduleMap.containsKey(currentNote.getCodemodule())) {
+                moduleMap.put(currentNote.getTitlemodule(), new ArrayList());
+            }
+            moduleMap.get(currentNote.getTitlemodule()).add(currentNote);
+            Log.i(TAG, "debug + " + currentNote.getTitlemodule());
+        }
+
+        ArrayList<String> displayArray = new ArrayList<String>();
+
+        for (ArrayList currentMarkModule: moduleMap.values()) {
+            Double averageMark = 0.0;
+            String nameModule = ((Note)currentMarkModule.get(0)).getTitlemodule();
+
+
+            for (Object currentMark: currentMarkModule) {
+                averageMark += ((Note)currentMark).getFinalNote();
+            }
+            averageMark /= currentMarkModule.size();
+            displayArray.add(nameModule + " : " + averageMark.toString());
+        }
+
+
+
+        ArrayAdapter<String> itemsAdapter =
+                new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, displayArray);
+
+        ((ListView)getActivity().findViewById(R.id.marks_list)).setAdapter(itemsAdapter);
+
+
+        Log.i(TAG, "keys : " + moduleMap.keySet());
     }
 }
